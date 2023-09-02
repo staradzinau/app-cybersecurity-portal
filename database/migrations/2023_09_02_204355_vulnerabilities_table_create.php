701b972bc2c4a1aea5d2e83a95586089c2da354a
Anton Staradzinau <staradzinau@gmail.com>
2023-09-02 23:17:56 +0200

    feat: Vulnerabilities - initial skeleton
    Pt. 1. DB layer. Add model + migration

---
+++ database/migrations/2023_09_02_204355_vulnerabilities_table_create.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Vulnerability;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(
+            Vulnerability::MAIN_TABLE_NAME,
+            function (Blueprint $table) {
+                $table->id();
+                $table->timestamps();
+                $table->string(Vulnerability::TITLE);
+                $table->text(Vulnerability::OVERVIEW);
+                $table->longText(Vulnerability::DESCRIPTION);
+                $table->longText(Vulnerability::PREVENTION_GUIDE);
+                $table->longText(Vulnerability::EXAMPLES_DESCRIPTION);
+            })
+        ;
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(Vulnerability::MAIN_TABLE_NAME);
+    }
+};
