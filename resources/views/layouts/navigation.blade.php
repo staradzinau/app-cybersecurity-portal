c43b91ac1494ca6558ca2dbcd651d5838e3f2260
Anton Staradzinau <staradzinau@gmail.com>
2023-09-03 09:29:53 +0200

    feat: UI - Vulnerabilities grid
    Pt. 2. Vulnerabilities grid
    Phase 1. Basic commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,6 +16,11 @@
                         {{ __('Dashboard') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-nav-link :href="route('vulnerability.index')" :active="request()->routeIs('vulnerability.index')">
+                        {{ __('Vulnerabilities') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
